0914e81a1e6f4febd1506478989dcbfe4631b331
Joel Leon M <109642410+JoelLeonUNS@users.noreply.github.com>
2025-06-25 10:59:10 -0500

    Update examen.sql

--- examen.sql
+++ examen.sql
@@ -19,9 +19,12 @@ CREATE TABLE Distrito (
   CONSTRAINT distrito_id_pk PRIMARY KEY (id)
 )
 
-INSERT INTO cliente VALUES ('73024631', 'Vitalmash', 'Bellavista','977896123',680.5, 1);
-INSERT INTO cliente VALUES ('73944735', 'Nido Pergolero', "Urb. Casuarinas P'4 Lt. 4", '935181370', 180.5. 1);
-INSERT INTO cliente VALUES ('73944739', 'Anidea', 'Francisco Bolognesi 1111', '960181410', 100, 2);
+INSERT INTO Cliente VALUES ('73024631', 'Vitalmash', 'Bellavista','977896123',680.5, 1);
+INSERT INTO Cliente VALUES ('73944735', 'Nido Pergolero', "Urb. Casuarinas P'4 Lt. 4", '935181370', 180.5. 1);
+INSERT INTO Cliente VALUES ('73944739', 'Anidea', 'Francisco Bolognesi 1111', '960181410', 100, 2);
 
-
+INSERT INTO Distrito VALUES (1, 'Nuevo Chimnbote');
+INSERT INTO Distrito VALUES (1, 'Chimnbote');
+INSERT INTO Distrito VALUES (1, 'Santa');
+COMMIT;
 
